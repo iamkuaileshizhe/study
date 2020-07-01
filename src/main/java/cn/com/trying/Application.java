2cee7b3dd10c47365f088b2d6dfdccb892cb3f14
Trying <kuaileshizhe_01@126.com>
2020-07-01 15:40:33 +0800

    删除无效引用

--- src/main/java/cn/com/trying/Application.java
+++ src/main/java/cn/com/trying/Application.java
@@ -1,22 +1,15 @@
 package cn.com.trying;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class Application  {
     public static void main(String[] args) {
         System.out.println("-------------------"+System.getProperty("os.name"));
         SpringApplication.run(Application.class, args);
+
+    
     }
 }
 
